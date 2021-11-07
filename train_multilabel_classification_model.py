d02f4f3a58a7fdd6b0ff0f34b9ff31e83b76ba3c
Tamer Tahamoqa <tamerthamoqa@gmail.com>
2021-11-07 12:02:23 +0200

    Update train_multilabel_classification_model.py

--- train_multilabel_classification_model.py
+++ train_multilabel_classification_model.py
@@ -3,7 +3,6 @@ import json
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import tensorflow.keras.backend as K
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
@@ -12,6 +11,7 @@ from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2
 from tensorflow.keras.applications.resnet import ResNet152
 from tensorflow.keras import optimizers
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from loss import compute_class_weights, set_binary_crossentropy_weighted_loss
 from utils import NumpyEncoder, plot_metrics
 
 
@@ -100,67 +100,6 @@ def set_model_architecture(model_architecture, image_height, image_width):
     print("Using {} model architecture.".format(model_architecture))
 
     return model
-
-
-def compute_class_weights(labels):
-    """
-    Note: Imported from the AI for Medicine Specialization course on Coursera: Assignment 1 Week 1.
-    Compute positive and negative weights for each class.
-
-    Args:
-        labels (np.array): matrix of labels, size (num_examples, num_classes)
-
-    Returns:
-        positive_weights (np.array): array of positive weights for each
-                                         class, size (num_classes)
-        negative_weights (np.array): array of negative weights for each
-                                         class, size (num_classes)
-    """
-    # total number of patients (rows).
-    N = labels.shape[0]
-
-    positive_frequencies = np.sum(labels, axis=0) / N
-    negative_frequencies = np.sum(labels == 0, axis=0) / N
-
-    positive_weights = negative_frequencies
-    negative_weights = positive_frequencies
-
-    return positive_weights, negative_weights
-
-
-def set_binary_crossentropy_weighted_loss(positive_weights, negative_weights, epsilon=1e-7):
-    """
-    Note: Imported from the AI for Medicine Specialization course on Coursera: Assignment 1 Week 1.
-    Returns weighted binary cross entropy loss function given negative weights and positive weights.
-
-    Args:
-      positive_weights (np.array): array of positive weights for each class, size (num_classes)
-      negative_weights (np.array): array of negative weights for each class, size (num_classes)
-
-    Returns:
-      weighted_loss (function): weighted loss function
-    """
-    def binary_crossentropy_weighted_loss(y_true, y_pred):
-        """
-        Returns weighted binary cross entropy loss value.
-
-        Args:
-            y_true (Tensor): Tensor of true labels, size is (num_examples, num_classes)
-            y_pred (Tensor): Tensor of predicted labels, size is (num_examples, num_classes)
-
-        Returns:
-            loss (Tensor): overall scalar loss summed across all classes
-        """
-        # initialize loss to zero
-        loss = 0.0
-
-        for i in range(len(positive_weights)):
-            # for each class, add average weighted loss for that class
-            loss += -1 * K.mean((positive_weights[i] * y_true[:, i] * K.log(y_pred[:, i] + epsilon) +
-                                 negative_weights[i] * (1 - y_true[:, i]) * K.log(1 - y_pred[:, i] + epsilon)))
-        return loss
-
-    return binary_crossentropy_weighted_loss
 
 
 def set_optimizer(optimizer, learning_rate, use_nesterov_sgd, use_amsgrad_adam):
@@ -252,8 +191,8 @@ def main():
     list_columns = list(train_df.columns)
     y_cols = list_columns[1::]  # First column is 'Path' column
 
-    training_dataset_mean = np.load("misc/training_dataset_mean_and_std_values/CheXpert_training_set_mean.npy")
-    training_dataset_std = np.load("misc/training_dataset_mean_and_std_values/CheXpert_training_set_std.npy")
+    training_dataset_mean = np.load("training_dataset_mean_and_std_values/CheXpert_training_set_mean.npy")
+    training_dataset_std = np.load("training_dataset_mean_and_std_values/CheXpert_training_set_std.npy")
 
     train_datagen = ImageDataGenerator(
         featurewise_center=True,  # Mean and standard deviation values of the training set will be loaded to the object
@@ -264,7 +203,7 @@ def main():
         fill_mode='constant',
         cval=0.0,
         horizontal_flip=False,  # Some labels would be heavily affected by this change if it is True
-        vertical_flip=False,  # Not suitable for Chest X-ray images if it is True
+        vertical_flip=False  # Not suitable for Chest X-ray images if it is True
     )
 
     # Set training dataset mean and std values for feature_wise centering and std normalization
@@ -342,7 +281,12 @@ def main():
                     multi_label=True
                 )
 
-                model = load_model(model_path)
+                model = load_model(
+                    model_path,
+                    custom_objects={
+                        "binary_crossentropy_weighted_loss": set_binary_crossentropy_weighted_loss
+                    }
+                )
                 # https://github.com/tensorflow/tensorflow/issues/45903#issuecomment-804973541
                 model.compile(optimizer=model.optimizer, metrics=[auc, accuracy], loss=loss)
 
@@ -354,7 +298,12 @@ def main():
                 multi_label=True
             )
 
-            model = load_model(model_path)
+            model = load_model(
+                model_path,
+                custom_objects={
+                    "binary_crossentropy_weighted_loss": set_binary_crossentropy_weighted_loss
+                }
+            )
             # https://github.com/tensorflow/tensorflow/issues/45903#issuecomment-804973541
             model.compile(optimizer=model.optimizer, metrics=[auc, accuracy], loss=loss)
 
